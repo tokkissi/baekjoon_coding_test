fc469154f3f1e39260308562fffbdaa8978522cd
tokkissi <alal4674@gmail.com>
2022-07-06 10:22:37 +0900

    1. 논리 개선, 각 조건 안에서 num 입력 받도록 함 2. 0으로 초기화 시에만 memset 사용 - 이전 버전의 오류 개선 3. toggle 의 경우 if 문이 아닌 XOR 배타적 논리합 을 사용, 속도 개선 4. empty 의 경우 cmd 입력 문이 문제가 없다는 가정 하에 else로 받아서 사용, 조건 하나 줄임

--- [11723] set.cpp
+++ [11723] set.cpp
@@ -4,41 +4,36 @@ using namespace std;
 int main(){
 	ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
 	int n, num;
 	string cmd;
-	int s[20]={0,};
+	int s[21]={0,};
 	
 	cin >> n;
 	
 	for(int i=0; i<n; i++){
 		cin >> cmd;
-		if(cmd=="all"){
-			memset(s, 2, sizeof(s));
-			continue;
-		}
-		else if(cmd=="empty"){
-			memset(s, 1, sizeof(s));
-			continue;
-		}
-		cin >> num;
-		
 		if(cmd=="add"){
-			s[num]=2;
-		} else if(cmd=="remove"){
+			cin >> num;
 			s[num]=1;
+		} else if(cmd=="remove"){
+			cin >> num;
+			s[num]=0;
 		} else if(cmd=="check"){
-			if(s[num]==0 || s[num]==1){
-					cout << 0 << "\n";
-				} else {
-					cout << 1 << "\n";
-				}
+			cin >> num;
+			if(s[num]){
+				cout << 1 << "\n";
+			} else {
+				cout << 0 << "\n";
+			}
 		} else if(cmd=="toggle"){
-			if(s[num]==0 || s[num]==1){
-					s[num]=2;
-				} else {
-					s[num]=1;
-				}
-		} 
+			cin >> num;
+			s[num]=s[num]^1;
+		} else if(cmd=="all"){
+			fill(s, s+21, 1);
+		} else {
+			memset(s, 0, sizeof(s));
+		}
 	}
 	
 	return 0;
