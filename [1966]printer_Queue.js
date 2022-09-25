43ff6c73764a898807c8ca46dc6b3427f90c3c34
tokkissi <alal4674@gmail.com>
2022-09-26 02:59:14 +0900

    프린터 큐. 구현 문제. 큐를 돌면서 구현하는 문제. 같은 숫자가 여러개일때 구현이 조금 까다로움

---
+++ [1966]printer_Queue.js
@@ -0,0 +1,73 @@
+let input = [];
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+readline.on("line", (line) => {
+  input.push(line);
+});
+
+readline.on("close", () => {
+  solution(input);
+  process.exit();
+});
+
+const solution = (inp) => {
+  let arr;
+  let ans = [];
+  let n;
+  let m;
+  for (let i = 1; i < inp.length; i++) {
+    if (i % 2 === 1) {
+      [n, m] = inp[i].split(" ").map((el) => Number(el));
+    }
+    if (i % 2 === 0) {
+      arr = inp[i].split(" ");
+      let q = [...arr];
+      let chk = Array(q.length).fill(0);
+      chk[m] = 1;
+      let target = Number(arr[m]);
+      let cnt = 0;
+
+      // 배열에 수가 남아있지 않거나 조건으로 탈출하기 전까지 계속 수행해라
+      while (q.length) {
+        let maxi = Math.max(...q);
+        let idx = q.indexOf(String(maxi));
+        let chkidx = chk.indexOf(1);
+
+        // 배열 중 최대값이 목표수가 아니라면 최대값이 맨 앞이 나올때까지 돌린다
+        if (maxi !== target) {
+          for (let i = 0; i < idx; i++) {
+            q.push(q.shift());
+            chk.push(chk.shift());
+          }
+          // 최대값이 목표값과 같다면
+        } else {
+          if (idx === chkidx) {
+            cnt++;
+            break; // 그 배열은 더 돌 필요 없다
+            // 찾은 최대값들 중 목표 요소가 맨 앞이 아니라면
+          } else {
+            let samecnt = 0;
+            for (let j = 0; j < chkidx; j++) {
+              if (q[j] === arr[m]) {
+                samecnt++;
+              }
+            }
+            cnt += samecnt + 1;
+            break;
+          }
+        }
+
+        // 맨 앞의 수는 배열 중 최대값이다. 빼주고 인쇄 수를 늘려주자
+        q.shift();
+        chk.shift();
+        cnt++;
+      }
+      ans.push(cnt);
+    }
+  }
+  console.log(ans.join("\n"));
+};
